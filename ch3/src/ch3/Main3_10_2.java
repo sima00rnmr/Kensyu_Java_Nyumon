8538e189a68cffd8d79509e1337d8d52bab57a87
m-tou <m-saito@gradito.net>
2026-02-03 18:16:09 +0900

    3_8_2途中（2/3 18:00　更新）

---
+++ ch3/src/ch3/Main3_10_2.java
@@ -0,0 +1,23 @@
+package ch3;
+
+public class Main3_10_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int isHungry = 0;
+		String food = ("りんご");
+		System.out.println("こんいちは！");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			if (isHungry == 1) {
+				System.out.println("はらぺこです");
+				System.out.println(food + "いただきます");
+
+			}
+		}
+		System.out.println("ごちそうさまでした");
+	}
+
+}
