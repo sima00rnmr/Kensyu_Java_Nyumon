457052e9b7ce5501a5b1685d2d8756f2466868a0
m-tou <m-saito@gradito.net>
2026-02-04 14:02:27 +0900

    20260204の14:00の進捗

--- ch4/src/ch5/Main5_3.java
+++ ch4/src/ch5/Main5_3.java
@@ -1,16 +1,23 @@
 package ch5;
 
-/*メインメソッド以外からのメソッド呼び出し*/
+/*メインメソッド以外からのメソッド呼び出し
+ * ①→④へ
+ * 先頭にmain以外のメソッドを置いてプログラムを実行しても、mainメソッドから動くことを確認する。
+ * */
 public class Main5_3 {
-
-	public static void methodA() {
-		System.out.println("methodA");
-		methodB();
+	//先頭にmain以外のメソッドを置いている
+	public static void methodA() { //　
+		System.out.println("methodA");//②
+		methodB();// methodBメソッドの呼び出し（引数）　③
 
 	}
 
 	public static void methodB() {
-		System.out.println("methodB");
+		System.out.println("methodB");//④
+	}
+
+	public static void main(String[] args) {
+		methodA();// methodAメソッドの呼び出し①
 	}
 
 }
---
+++ ch4/src/ch5/Main5_4.java
@@ -0,0 +1,19 @@
+package ch5;
+
+public class Main5_4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("メソッドを呼び出します");
+		hello("湊");
+		hello("朝香");
+		hello("菅原");
+		System.out.println("メソッドを呼び出しが完了しました");
+	}
+
+	public static void hello(String name) {
+		System.out.println(name + "さん、こんにちは");
+
+	}
+}
