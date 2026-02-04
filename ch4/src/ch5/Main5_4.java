f3c07ae1ea1f025bb72fadde4b198b8de1fe6a86
m-tou <m-saito@gradito.net>
2026-02-04 14:24:03 +0900

    5_5 まで終了

--- ch4/src/ch5/Main5_4.java
+++ ch4/src/ch5/Main5_4.java
@@ -1,18 +1,22 @@
 package ch5;
 
+/*引数を１つだけ渡す
+ * ※引数　メソッドをよびだすさいに、呼び出し元から値を渡す事ができる。
+ * この時に渡される値の事
+ * */
 public class Main5_4 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
 		System.out.println("メソッドを呼び出します");
-		hello("湊");
-		hello("朝香");
-		hello("菅原");
+		hello("湊");//"湊"を渡してhelloメソッドを呼び出す
+		hello("朝香");//"朝香"を渡してhelloメソッドを呼び出す
+		hello("菅原");//"菅原"を渡してhelloメソッドを呼び出す
 		System.out.println("メソッドを呼び出しが完了しました");
 	}
 
-	public static void hello(String name) {
+	public static void hello(String name) {//hello()の中身を引っ張ってくるよ、以下（）で値を使うことが出来るよ、の意
 		System.out.println(name + "さん、こんにちは");
 
 	}
---
+++ ch4/src/ch5/Main5_5.java
@@ -0,0 +1,24 @@
+package ch5;
+
+/*5_5
+ * 複数の引数を渡す
+ *  
+ * 
+ * */
+public class Main5_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		add(100, 20);//…①
+		add(200, 50);//…②
+
+	}
+
+	//複数の引数を受け取るaddメソッド
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);//①、②それぞれの処理の結果が出てくる（①100+20,②200+50）
+	}
+
+}
