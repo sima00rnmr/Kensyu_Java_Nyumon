17a9d1cce8bdbec8f7bd6ded87f1063b0705a0dd
m-tou <m-saito@gradito.net>
2026-02-05 18:55:21 +0900

    8_4まで空データ含め作成

--- ch8/Main8_1.java
+++ ch8/Main8_1.java
@@ -2,7 +2,12 @@
 public class Main8_1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//(以下の内容をJavade記述していく)
+		//勇者よ、この仮想世界に生まれよ！
+		//お化けキノコよ、この仮想世界に生まれよ！
+		//勇者よ、戦え！
+		//お化けキノコよ、逃げろ！
+		
 
 	}
 
---
+++ ch8/Main8_2.java
@@ -0,0 +1,29 @@
+/*8_2
+ * HeroクラスをJavannoコードで表したもの
+ * 
+ * 
+ * 
+ * 
+ * */
+
+public class Main8_2 {//Heroクラス
+	//属性の定義
+	String name;
+	int hp;
+	// 操作の定義
+	pblic void attack() {
+		
+	}
+	public void sleep() {
+		
+	}
+	public void sit() {
+		}
+	public void slip() {
+		
+}
+	public void run() {
+		
+	}
+	
+}
