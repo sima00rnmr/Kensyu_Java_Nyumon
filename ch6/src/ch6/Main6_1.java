65967c7349b5f3ea932b62753b0d8a5270ce5f38
m-tou <m-saito@gradito.net>
2026-02-05 14:39:41 +0900

    6_3まで空のクラス作成（同時並行で作業するため）

--- ch6/src/ch6/Main6_1.java
+++ ch6/src/ch6/Main6_1.java
@@ -1,5 +1,21 @@
 package ch6;
-
+/*計算機プログラム
+ * メソッドtasu 
+ * メソッドhiku
+ *　数値は　a=10 b=2 ←引数で作成 
+ */
 public class Main6_1 {
-
+	public static void main(String[] args) {
+		int a =10;
+		int b =2;
+		int total = (a+b);
+		int delta = (a-b);
+		System.out.println("足すと"+ total +"、引くと"+ delta);	
+		}
+public static int tasu(int a, int b) {
+	return (a+b);
+}
+public static int hiku(int a,int b) {
+	return(a-b);
+ }
 }
---
+++ ch6/src/ch6/Main6_2.java
@@ -0,0 +1,10 @@
+package ch6;
+
+public class Main6_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
