1fcae348cc6ce8390e433c01651c82d44d857c1f
m-tou <m-saito@gradito.net>
2026-02-09 18:52:58 +0900

    追加

--- ch10/src/ch10/PoisonMatango.java
+++ ch10/src/ch10/PoisonMatango.java
@@ -1,5 +1,19 @@
 package ch10;
 
-public class PoisonMatango {
+public class PoisonMatango extends Matango{
+	int PoisonAttack;
+	
+	// 毒攻撃が出来る
+	public void PoisonAttack() {
+		
+	}
+	
+	
+	//PoisonMatango pm = new PoisonMatango('A')でインスタンス化出来るクラスとする
+	
+	//PoisonMatangoは毒を用いた攻撃が可能な限り残り回数をintフィールドとして持っており、初期値は5
+	
+	//
+
 
 }
