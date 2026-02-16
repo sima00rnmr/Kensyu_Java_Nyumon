0385e3c6afcacb4456f3d9ae260806e946987e0a
m-tou <m-saito@gradito.net>
2026-02-16 19:03:58 +0900

    追加箇所

--- ch12/src/ch12/Main12_ren.java
+++ ch12/src/ch12/Main12_ren.java
@@ -3,11 +3,11 @@ package ch12;
 public class Main12_ren {
 
 	public static void main(String[] args) {
-		Y[] y = new Y[2];
+		Y[] array = new Y[2];
 		
-		y[0] = new A();
-		y[0] = new B();
-		for (Y y:y) {
+		array[0] = new A();
+		array[0] = new B();
+		for (Y y: array) {
 			y.b();
 		}
 		
