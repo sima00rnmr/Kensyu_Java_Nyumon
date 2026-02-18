11d65f602d51c53753a7a3024aaa10453a8f67c3
m-tou <m-saito@gradito.net>
2026-02-18 18:56:40 +0900

    本日最後のコミット

--- ch17/src/ch17/Main17_6.java
+++ ch17/src/ch17/Main17_6.java
@@ -1,17 +1,24 @@
 package ch17;
+
 import java.io.FileWriter;
+
 public class Main17_6 {
 	public static void main(String[] args) {
-		FileWriter fw =new FileWriter("data.txt");
+		FileWriter fw = null;
 		try {
+			fw = new FileWriter("data.txt");
 			fw.write("hello!");
-			
-		}	catch(Exception e){
+
+		} catch (Exception e) {
 			System.out.println("何らかの例外が発生しました");
-		}finally {
-			fw.close();
+		} finally {
+			try {
+				fw.close(); //close失敗時には特に何もしない
+			} catch (I0Exception e) {
+				; // 何もしない、の明示
+			}
+
 		}
-		
 
 	}
 
